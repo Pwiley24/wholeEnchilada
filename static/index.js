d828e880b77662ce46ace2d6eed993a783da1b8c
wileypa <wileypa@oregonstate.edu>
2025-02-13 15:13:30 -0800

    added to the review form

--- static/index.js
+++ static/index.js
@@ -1,21 +1,50 @@
-//JS for Testimony filtering:
-//JS outline from assignment 5 --> source citation
-var allTestimonies = []
-var testimonyElems = document.getElementsByClassName('testimonial-post')
+/*
+ * Applies the filters currently entered by the user to the set of all recipes.
+ * Any recipe that satisfies the user's filter values will be displayed,
+ * including recipes that are not currently being displayed because they didn't
+ * satisfy an old set of filters. Recipes that don't satisfy the filters are
+ * removed from the DOM.
+ */
+function doFilterUpdate() {
+    //Grab values of filters from user inputs.
+    var filters = {
+        text: document.getElementById('filter-text').value.trim(),
+        cuisine: document.getElementById()
+    }
+    
+    var testimonyContainer = document.getElementById('testimonies-flex')
+    var testimonyChildren = testimonyContainer.children
 
-for (var i = 0; i < testimonyElems.length; i++) {
-    console.log("Inspecting testimony element: ", testimonyElems[i]);  // Check the DOM element
-    allTestimonies.push(parseTestimonyElem(testimonyElems[i]));
+    // Reset testimony elements back to normal by making them visible again
+    for (var j = 0; j < testimonyChildren.length;j++) {
+        if (testimonyChildren[j].classList.contains('hidden')) {
+            testimonyChildren[j].classList.remove('hidden')
+        }
+    }
+
+    /*
+     * "Remove" all "testimony" elements by hiding them.
+     */ 
+    var i = 0
+    allTestimonies.forEach(function (testimony) {
+        if (!(testimonyPassesFilters(testimony, filters))) {
+            testimonyChildren[i].classList.add('hidden')
+        }
+        i++
+    })
+    
 }
+
+
+
+
+
 
 //Check if the filter button exists
 var filterUpdateButton = document.getElementById('filter-update-button');
 
 if (filterUpdateButton) {
-    console.log("filter button: ", filterUpdateButton)
-
     filterUpdateButton.addEventListener('click', function() {
-        console.log('Filter button clicked');
         doFilterUpdate(); 
     });
 }
