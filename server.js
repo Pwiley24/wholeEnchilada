84336ba7f92d7ced0d6cc5de0aeb431f2a109f50
stepjaso <stepjaso@oregonstate.edu>
2025-02-26 23:17:17 -0800

    Changed MySQL connection to be pooled, added delete functionality to recipes, began adding details for ingredients used in recipes to the Recipes page.

--- server.js
+++ server.js
@@ -5,25 +5,35 @@ var express = require('express');
 var exphbs = require('express-handlebars');
 var fs = require('fs');
 var Handlebars = require('handlebars');
-app.use(express.json());
+//app.use(express.json());
 var port = process.env.PORT || 3033;
 
 
 const mysql = require('mysql2');
-const pool = mysql.createConnection({
-    host: "classmysql.engr.oregonstate.edu",
-    user: "cs340_wileypa",
-    password: "0573",
-    database: "cs340_wileypa",
-    multipleStatements: true
+const pool = mysql.createPool({
+    // host: "classmysql.engr.oregonstate.edu",
+    // user: "cs340_wileypa",
+    // password: "0573",
+    // database: "cs340_wileypa",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    multipleStatements: true,
+    waitForConnections: true,
+    connectionLimit: 10,
+    maxIdle: 10,
+    idleTimeout: 60000,
+    queueLimit: 0
   });
 
-  pool.connect((err) => {
+  pool.getConnection((err, connection) => {
     if (err) {
         console.error('Error connecting to the database:', err.stack);
         return;
     }
     console.log('Connected to the database')
+    connection.release();
   });
 
   module.exports = pool;
@@ -50,7 +60,7 @@ app.get('', function (req, res, next) {
 
 // Display Recipes page
 app.get('/recipes', function (req, res, next) {
-    pool.connect((err) => {
+    pool.getConnection((err, connection) => {
         if (err) throw err;
 
         pool.query("SELECT cuisine_ID, name FROM Cuisines; SELECT Recipes.recipe_ID AS 'recipe_ID', Recipes.name AS 'recipe_name', Recipes.description AS 'recipe_description', Cuisines.name AS 'cuisine_name' FROM Recipes JOIN Cuisines ON Recipes.cuisine_ID = Cuisines.cuisine_ID; SELECT ingredient_ID, name FROM Ingredients", [1, 2], (err, results, fields) => {
@@ -58,12 +68,34 @@ app.get('/recipes', function (req, res, next) {
             //res.send(results);
             res.status(200).render("recipes", { cuisineList: results[0], recipes: results[1], ingredientList: results[2] });
         });
+        connection.release();
+    });
+});
+
+app.delete('/deleteRecipe/:recipeid', function(req, res, next) {
+    pool.getConnection((err, connection) => {
+        if (err) throw err;
+        pool.query("DELETE FROM Recipes WHERE recipe_ID = ?", [req.params.recipeid], (err, results) => {
+            if (err) throw err;
+        });
+        connection.release();
+        res.sendStatus(204);
+    })
+});
+
+app.get('/getRecipeById/:recipeid', function(req, res, next) {
+    pool.getConnection((err, connection) => {
+        if (err) throw err;
+        pool.query("SELECT ir.recipe_ID, ir.ingredient_ID, r.name AS 'recipe_name', i.name AS 'ingredient_name', ingredient_qty, ingredient_qty_display_uom, ingredient_qty_to_gram  FROM IngredientsOfRecipes ir JOIN Recipes r ON ir.recipe_ID = r.recipe_ID JOIN Ingredients i ON ir.ingredient_ID = i.ingredient_ID WHERE ir.recipe_ID = ?", [req.params.recipeid], (err, results) => {
+            if (err) throw err;
+            res.status(200).send(results);
+        })
     });
 });
 
 // Display Ingredients page
 app.get('/ingredients', function (req, res, next) {
-    pool.connect((err) => {
+    pool.getConnection((err, connection) => {
         if (err) throw err;
 
         pool.query("SELECT ingredient_ID, name, cost_per_gram FROM Ingredients;", (err, results, fields) => {
@@ -71,6 +103,7 @@ app.get('/ingredients', function (req, res, next) {
             //res.send(results);
             res.status(200).render("ingredients", { ingredients: results });
         });
+        connection.release();
     });
 });
 
@@ -91,27 +124,29 @@ app.get('/reviews', function (req, res, next) {
     res.status(200).render("reviews");
 });
 
-// get cuisines
+// get cuisines list
 app.get('/getCuisineList', function(req, res) {
-    pool.connect((err) => {
+    pool.getConnection((err, connection) => {
         if (err) throw err;
 
         pool.query("Select cuisine_ID, name from Cuisines", (err, results, fields) => {
             if (err) throw err;
             res.send(results);
         });
+        connection.release();
     });
 });
 
 // get ingredients
 app.get('/getIngredientList', function(req, res) {
-    pool.connect((err) => {
+    pool.getConnection((err, connection) => {
         if (err) throw err;
 
         pool.query("Select ingredient_ID, name from Ingredients", (err, results, fields) => {
             if (err) throw err;
             res.send(results);
         });
+        connection.release();
     });
 });
 
