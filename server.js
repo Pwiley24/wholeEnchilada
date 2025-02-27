905f27e1d3891bce5adaf238b085cc6244e5659b
Pwiley24 <97415028+Pwiley24@users.noreply.github.com>
2025-02-27 06:55:54 -0800

    Update server.js

--- server.js
+++ server.js
@@ -11,10 +11,6 @@ var port = process.env.PORT || 3033;
 
 const mysql = require('mysql2');
 const pool = mysql.createPool({
-    // host: "classmysql.engr.oregonstate.edu",
-    // user: "cs340_wileypa",
-    // password: "0573",
-    // database: "cs340_wileypa",
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
